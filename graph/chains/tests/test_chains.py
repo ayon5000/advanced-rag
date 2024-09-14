cb04fb7b2e9d657c4618b11a116d07dd789623de
Ayon Banerjee <ayon5000@gmail.com>
2024-09-14 20:48:19 +0530

    add grader chain, node and grader tests

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -1,2 +1,30 @@
-def test__foo()-> None:
-    assert 1 == 1
+from dotenv import load_dotenv
+
+_ = load_dotenv()
+
+from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
+from ingestion import retriever
+
+
+def test__retrieval_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": question, "document": doc_txt}
+    )
+
+    assert res.binary_score == "yes"
+
+
+def test__retrieval_grader_answer_no() -> None:
+    question = "how to make pizza"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": question, "document": doc_txt}
+    )
+
+    assert res.binary_score == "no"
