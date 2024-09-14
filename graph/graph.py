5578d123bc33a533a582d96e2b6385c03387b08d
Ayon Banerjee <ayon5000@gmail.com>
2024-09-15 02:01:08 +0530

    build graph

---
+++ graph/graph.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+from langgraph.graph import StateGraph, END
+from graph.consts import GENERATE, GRADE_DOCUMENTS, RETRIEVE, WEBSEARCH
+from graph.state import GraphState
+from graph.nodes import retrieve, generate, grade_documents, web_search
+
+_ = load_dotenv()
+
+
+def decide_to_generate(state: GraphState):
+    print("---ASSESS GRADED DOCUMENTS---")
+
+    if state["web_search"]:
+        print(
+            "---DECISION: NOT ALL DOCUMENTS ARE NOT RELEVANT TO QUESTION, INCLUDE WEB SEARCH---"
+        )
+        return WEBSEARCH
+    else:
+        print("---DECISION: GENERATE---")
+        return GENERATE
+
+
+workflow: StateGraph = StateGraph(GraphState)
+
+workflow.add_node(RETRIEVE, retrieve)
+workflow.add_node(GRADE_DOCUMENTS, grade_documents)
+workflow.add_node(WEBSEARCH, web_search)
+workflow.add_node(GENERATE, generate)
+
+workflow.set_entry_point(RETRIEVE)
+workflow.add_edge(RETRIEVE, GRADE_DOCUMENTS)
+workflow.add_conditional_edges(
+    GRADE_DOCUMENTS,
+    decide_to_generate,
+    path_map={
+        WEBSEARCH: WEBSEARCH,
+        GENERATE: GENERATE,
+    },
+)
+workflow.add_edge(WEBSEARCH, GENERATE)
+workflow.add_edge(GENERATE, END)
+
+app = workflow.compile()
+
+app.get_graph().draw_mermaid_png(output_file_path="graph.png")
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
+from graph.graph import app
 
 _ = load_dotenv()
 
 if __name__ == "__main__":
     print("Hello Advanced RAG")
+    response = app.invoke({"question": "What is agent memory?"})
+    print(response)
